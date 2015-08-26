8a57ebfec2d9e50e41a2f9c0a0bbad5732ab261d
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-08-26 18:03:12 -0400

    Updated dynamic data

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing telescope',
-    url: 'http://google.com'
-  },
-  {
-    title: 'Twitter',
-    url: 'http://twitter.com'
-  },
-  {
-    title: 'Robert',
-    url: 'http://robraven.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
